2b0cf8923a0ad5ce1aec7a25390c7c81d4b21693
吴乐川 <wulechuan@live.com>
2022-04-29 20:04:04 +0800

    命令行脚本改进。

--- 用于研发阶段的命令行工具/PowerShell/Update-更新所有npm依赖包至最新版本.ps1
+++ 用于研发阶段的命令行工具/PowerShell/Update-更新所有npm依赖包至最新版本.ps1
@@ -1,60 +1,88 @@
 #
 
-# ------------------------------------------------------
-# ---------- 【产品依赖包】 ------------------------------
-# ------------------------------------------------------
+# -------------------------------------------------------
+#           特定版本之【产品依赖包】
+# -------------------------------------------------------
 
-# 以下均为须采用特定版本的【产品依赖包】。
-# Write-Host
-# Write-Host
-# Write-Host
-# Write-Host  -NoNewline  -F 'DarkRed'                '===== npm i    '
-# Write-Host  -NoNewline  -F 'White'    -B 'DarkRed'  '特定版本'
-# Write-Host              -F 'DarkRed'                '的【产品依赖包】 ============================'
-# Write-Host
-# 暂无。
+Write-Host
+
+Write-Host  -NoNewline  -F 'DarkRed'                '===== npm i    '
+Write-Host  -NoNewline  -F 'White'    -B 'DarkRed'  '特定版本'
+Write-Host              -F 'DarkRed'                '之【产品依赖包】 ============================'
+
+Write-Host
+
+Write-Host '暂无。'
 
 
 
-# 以下均为可采用 latest 版本的【产品依赖包】。顺便提醒，虽然一般而言 latest 版本应恰为最高版本，但并不确保。
+
+
+# -------------------------------------------------------
+#          最末版本之【产品依赖包】
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# 顺便提醒，虽然一般而言 latest 版本应恰为最高版本，但并不确保。
+# -------------------------------------------------------
+
 Write-Host
 Write-Host
 Write-Host
+Write-Host
+Write-Host
+
 Write-Host  -NoNewline  -F 'DarkRed'                '===== npm i    '
 Write-Host  -NoNewline  -F 'DarkRed'                '最末版本'
-Write-Host              -F 'DarkRed'                '的【产品依赖包】 ============================'
+Write-Host              -F 'DarkRed'                '之【产品依赖包】 ============================'
+
 Write-Host
+
 npm  i 'de-indent@latest'
 
 
 
 
 
-# ------------------------------------------------------
-# ---------- 【研发依赖包】 ------------------------------
-# ------------------------------------------------------
+# -------------------------------------------------------
+#           特定版本之【研发依赖包】
+# -------------------------------------------------------
 
-# 以下均为须采用特定版本的【研发依赖包】。
-# Write-Host
-# Write-Host
-# Write-Host
-# Write-Host  -NoNewline  -F 'DarkGreen'              '===== npm i -D '
-# Write-Host  -NoNewline  -F 'Black'      -B 'Green'  '特定版本'
-# Write-Host              -F 'DarkGreen'              '的【研发依赖包】 ============================'
-# Write-Host
+Write-Host
+Write-Host
+Write-Host
+Write-Host
+Write-Host
+
+Write-Host  -NoNewline  -F 'DarkGreen'              '===== npm i -D '
+Write-Host  -NoNewline  -F 'Black'      -B 'Green'  '特定版本'
+Write-Host              -F 'DarkGreen'              '之【研发依赖包】 ============================'
+
+Write-Host
+
 npm  i  -D `
     'vue@2'
 
 
 
-# 以下均为可采用 latest 版本的【研发依赖包】。顺便提醒，虽然一般而言 latest 版本应恰为最高版本，但并不确保。
+
+
+# -------------------------------------------------------
+#          最末版本之【研发依赖包】
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# 顺便提醒，虽然一般而言 latest 版本应恰为最高版本，但并不确保。
+# -------------------------------------------------------
+
 Write-Host
 Write-Host
 Write-Host
+Write-Host
+Write-Host
+
 Write-Host  -NoNewline  -F 'DarkGreen'              '===== npm i -D '
 Write-Host  -NoNewline  -F 'DarkGreen'              '最末版本'
-Write-Host              -F 'DarkGreen'              '的【研发依赖包】 ============================'
+Write-Host              -F 'DarkGreen'              '之【研发依赖包】 ============================'
+
 Write-Host
+
 npm  i  -D `
     '@babel/core@latest' `
     '@babel/node@latest' `
@@ -69,3 +97,17 @@ npm  i  -D `
     'fs-extra@latest' `
     'typescript@latest' `
     'vue-property-decorator@latest'
+
+
+
+
+
+# -------------------------------------------------------
+#           结束
+# -------------------------------------------------------
+
+Write-Host
+Write-Host
+Write-Host
+Write-Host
+Write-Host
